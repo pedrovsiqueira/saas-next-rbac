a722868eb340d43f88882d0cdbf29aaa1b2e8649
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-12-01 06:01:33 -0300

    feat: finish authentication with email and password

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,6 +1,8 @@
 'use server'
 
 import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
@@ -28,7 +30,12 @@ export async function signInWithEmailAndPassword(data: FormData) {
       password,
     })
 
-    console.log({ token })
+    const cookieStore = await cookies()
+
+    cookieStore.set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
@@ -44,5 +51,5 @@ export async function signInWithEmailAndPassword(data: FormData) {
     }
   }
 
-  return { success: true, message: null, errors: null }
+  redirect('/')
 }
