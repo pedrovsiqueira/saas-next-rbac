051f6ea36aa56ceb1de53d12045d7a78dc902a14
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2024-12-27 16:13:59 -0300

    feat: create logic to handle permissions for memberships

--- apps/api/src/http/routes/orgs/get-membership.ts
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -24,6 +24,7 @@ export async function getMembership(app: FastifyInstance) {
               membership: z.object({
                 id: z.string().uuid(),
                 role: roleSchema,
+                userId: z.string().uuid(),
                 organizationId: z.string().uuid(),
               }),
             }),
@@ -39,9 +40,11 @@ export async function getMembership(app: FastifyInstance) {
           membership: {
             id: membership.id,
             role: membership.role,
+            userId: membership.userId,
             organizationId: membership.organizationId,
           },
         }
-      },
+        // eslint-disable-next-line prettier/prettier
+      }
     )
 }
